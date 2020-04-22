022b3e3f70d3b7d969bf4b383e9d7818d237ee67
macbook <macbook@macbooks-MacBook-Pro.local>
2020-04-22 00:03:06 -0700

    #1048

---
+++ volume1/1048.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+    int n;  
+    scanf("%d", &n);
+
+    vector<int> a,b;
+
+    int x,y;
+
+    for(size_t i=0; i < n; ++i){
+        scanf("%d %d", &x, &y);
+        a.push_back(x);
+        b.push_back(y);
+    }
+
+    int sum = 0;
+    int carry = 0;
+
+    vector<int> ans(n);
+
+    for(int i = a.size() - 1; i >= 0; --i){
+        sum = a[i] + b[i] + carry;
+
+        carry = sum / 10;
+        sum = sum % 10;
+
+        ans[i] = sum;
+    }
+
+    if(carry != 0){
+        cout << carry;
+    }
+
+    for(size_t i = 0; i < ans.size(); ++i){
+        printf("%d", ans[i]);
+    }
+    cout << endl;
+
+    return 0;
+}
