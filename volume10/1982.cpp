93b42137f5ff3d798386d26d8b04c76c5ce4cf2f
macbook <macbook@AIs-MacBook-Pro.local>
2020-08-30 17:51:56 +0600

    upd

--- volume10/1982.cpp
+++ volume10/1982.cpp
@@ -35,6 +35,7 @@ typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef vector<bool> vb;
 typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
 typedef tuple<int, int, int> tp;
 
 void print(vector<int> &a){
@@ -43,63 +44,122 @@ void print(vector<int> &a){
     }
     cout << endl;
 }
+
 int n, k;
-vector<vi> gr;
-vector<vll> dist;
-vi cities;
-vb used;
-const int INF = 1e9;
+vector<pair<ll, pll> > edges;
+vector<tp> mst;
+vi tree_id;
+vb cities;
+vb exist;
+ll cost = 0;
 
-void floyd_warshell(){
-    dist = vector<vll> (n, vll(n, INF));
-    rep(i, 0, n){
-        rep(j, 0, n){
-            if(i == j) continue;
-            dist[i][j] = gr[i][j];
-        }
-    }
+void kruskal(){
 
-    rep(k, 0, n){
-        rep(i, 0, n){
+    tree_id = vi(n);
+
+    sort(all(edges));
+
+    rep(i, 0, n)
+        tree_id[i] = i;
+    
+    // for(auto e : edges){
+    //     int a,b,w;
+    //     a = e.se.fi; b = e.se.se; w = e.fi;
+
+    //     cout << a + 1 << " " << b + 1 << " " << w << endl;
+    // }
+    // return;
+
+    for(auto e : edges){
+        int a,b,w;
+        a = e.se.fi; b = e.se.se; w = e.fi;
+
+        if(tree_id[a] != tree_id[b] && ((cities[a] && !cities[b]) || 
+                (cities[b] && !cities[a]))){
+            mst.pb({a,b,w});
+            cost += w;
+            cities[b] = cities[a] = true;
+            int old_id = tree_id[b];
+            int new_id = tree_id[a];
             rep(j, 0, n){
-                if(i != j){
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                if(tree_id[j] == old_id){
+                    tree_id[j] = new_id;
                 }
             }
         }
+    }
+}
+
+const int INF = 1e9;
+vector<vi> g;
+
+void prim(){
+    vector<bool> used (n);
+    vector<int> min_e (n, INF), sel_e (n, -1);
+    min_e[0] = 0;
+    for (int i=0; i<n; ++i) {
+        int v = -1;
+        for (int j=0; j<n; ++j)
+            if (!used[j] && (v == -1 || min_e[j] < min_e[v]))
+                v = j;
+
+        if (min_e[v] == INF) {
+            exit(0);
+        }
+    
+        used[v] = true;
+        if (sel_e[v] != -1){
+            if((cities[v] && !cities[sel_e[v]]) || 
+            (!cities[v] && cities[sel_e[v]])){
+                cost += min_e[v];
+                cities[v] = cities[sel_e[v]] = true;
+            }
+        }
+    
+        for (int to=0; to<n; ++to)
+            if (g[v][to] < min_e[to] && g[v][to] > 0) {
+                min_e[to] = g[v][to];
+                sel_e[to] = v;
+            }
     }
 }
 void test_case(){
     cin >> n >> k;
-    cities = vi(k);
-    used = vb(n, false);
-    gr = vector<vi> (n, vi(n));
+    cities = vb(n, false);
 
-    rep(i, 0, k) cin >> cities[i], used[cities[i]-1] = true;
+    g = vector<vi> (n, vi(n));
 
+    int x; 
+    rep(i, 0, k){
+        cin >> x;
+        cities[x-1] = true;
+    }
+
+    // int w;
     rep(i, 0, n){
         rep(j, 0, n){
-            cin >> gr[i][j];
+            // cin >> w;
+            // if(i==j) continue;
+            // edges.pb({w, {i,j}});
+            cin >> g[i][j];
         }
     }
-    floyd_warshell();
+    // kruskal();
 
-    ll bill = 0;
-    rep(i, 0, n){
-        if(!used[i]){
-            ll mini = dist[cities[0]-1][i];
-            rep(j, 1, cities.size()){
-                if(dist[cities[j]-1][i] < mini){
-                    mini = dist[cities[j]-1][i];
-                }
-            }
-            used[i] = true;
-            cities.push_back(i+1);
-            bill += mini;
-        }
-    }
-    cout << bill << endl;
+    // rep(i, 0, n){
+    //     cout << cities[i] << " ";
+    // }
+    // cout << endl;
+
+    // for(auto e : mst){
+    //     int a, b, w;
+    //     tie(a,b,w) = e;
+    //     cout << a + 1 << " " << b + 1 << " " << w << endl;
+    // }
+    prim();
+    cout << cost << endl;
 }
+
 
 int main(){
 
